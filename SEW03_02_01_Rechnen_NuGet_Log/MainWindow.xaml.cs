474b657970b6201ea29fab771313b6fd9687a7b3
JonasUllmann <jonas.ullmann@josephinum.at>
2024-09-06 09:21:01 +0200

    logfile nicht gefunden

--- SEW03_02_01_Rechnen_NuGet_Log/MainWindow.xaml.cs
+++ SEW03_02_01_Rechnen_NuGet_Log/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NLog;
 
 namespace SEW03_02_01_Rechnen_NuGet_Log
 {
@@ -16,9 +17,54 @@ namespace SEW03_02_01_Rechnen_NuGet_Log
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        int num1;
+        int num2;
+        int erg;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            logger.Info("pressed addbutton");
+
+
+
+            try
+            {
+                logger.Info("num1 convert start");
+                num1 = Convert.ToInt32(tbxnum1.Text);
+
+                logger.Info("num2 convert start");
+                num2 = Convert.ToInt32(tbxnum2.Text);
+
+            }
+
+            catch(Exception ex)
+            {
+                logger.Error("convert fehlgeschlagen");
+                tbkerg.Text = ex.ToString();
+
+            }
+
+            
+
+            try
+            {
+                logger.Info("add");
+                erg = num1 + num2;
+            }
+            catch(Exception ex) 
+            {
+                logger.Error("add fehlgeschlagen");
+            };
+
+            tbkerg.Text = erg.ToString();
+
+
+
         }
     }
 }
