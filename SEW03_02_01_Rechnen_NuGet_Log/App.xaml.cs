04c297ea9223be41e217d1fca4fa8260183e527e
j.ullmann <j.ullmann@ITLAB1C09>
2024-09-06 08:17:45 +0200

    Add project files.

---
+++ SEW03_02_01_Rechnen_NuGet_Log/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SEW03_02_01_Rechnen_NuGet_Log
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
