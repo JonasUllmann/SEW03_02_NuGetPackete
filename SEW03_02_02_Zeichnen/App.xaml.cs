0bcdb0a6f5e2ffc0cb5751b78b6c3c2dc0106292
Jonas Ullmann <jonas.ullmann@josephinum.at>
2024-09-13 10:29:50 +0200

    woopec begonnen

---
+++ SEW03_02_02_Zeichnen/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SEW03_02_02_Zeichnen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ SEW03_02_02_Zeichnen/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Woopec.Core;
+
+namespace SEW03_02_02_Zeichnen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static void TurtleMain()
+        {
+
+            var seymour = Turtle.Seymour();
+
+            seymour.Left(45);
+            seymour.Forward(100);
+            seymour.Right(45);
+            seymour.Forward(50);
+        }
+    }
+}
