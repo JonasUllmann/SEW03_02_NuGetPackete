0c30a20a5170684ce8d71c8fa511f58421d7b8dd
JonasUllmann <jonas.ullmann@josephinum.at>
2024-09-15 20:35:21 +0200

    restliche Buchstaben, verschiedene Farben

--- SEW03_02_02_Zeichnen/MainWindow.xaml.cs
+++ SEW03_02_02_Zeichnen/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace SEW03_02_02_Zeichnen
 
             var will = Turtle.Seymour();
 
-            will.Speed = 5;
+            will.Speed = 25;
 
             will.PenUp();
             will.GoTo((-225, 150));
@@ -35,9 +35,29 @@ namespace SEW03_02_02_Zeichnen
             j();
 
             will.PenUp();
-            will.GoTo((-125, 50));
+            will.GoTo((-150, 35));
+            will.PenColor = new Woopec.Core.Color(255, 0, 0);
 
+            o();
 
+            will.PenUp();
+            will.GoTo((-25, 35));
+            will.PenColor = new Woopec.Core.Color(0, 0, 255);
+
+            n();
+
+            will.PenUp();
+            will.GoTo((125, 35));
+            will.PenColor = new Woopec.Core.Color(255, 0, 255);
+
+            a();
+
+            will.PenUp();
+            will.GoTo((325, 5));
+            will.PenColor = new Woopec.Core.Color(150, 53, 75);
+
+            s();
+            will.PenColor = new Woopec.Core.Color(12, 200, 100);
 
             void j()
             {
@@ -60,6 +80,108 @@ namespace SEW03_02_02_Zeichnen
             void o()
             {
 
+                will.PenDown();
+                will.SetHeading(0);
+
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(50);
+            }
+
+            void n()
+            {
+
+                will.PenDown();
+                will.SetHeading(270);
+
+                will.Forward(115);
+
+                will.Right(180);
+                will.Forward(90);
+
+                will.Right(45);
+                will.Forward(20);
+
+                will.Right(45);
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(20);
+
+                will.Right(45);
+                will.Forward(94);
+
+            }
+
+            void a()
+            {
+
+                o();
+                will.PenUp();
+                will.SetHeading(0);
+                
+                will.Forward(85);
+                will.PenDown();
+
+                will.Right(90);
+                will.Forward(125);
+                
+
+            }
+
+            void s()
+            {
+
+                will.SetHeading(135);
+                will.PenDown();
+
+                will.Forward(22);
+
+                will.Left(45);
+                will.Forward(50);
+
+                will.Left(45);
+                will.Forward(22);
+
+                will.Left(45);
+                will.Forward(22);
+
+                will.Left(45);
+                will.Forward(22);
+
+                will.Left(45);
+                will.Forward(50);
+
+                will.Right(45);
+                will.Forward(22);
+
+                will.Right(45);
+                will.Forward(22);
+
+                will.Right(45);
+                will.Forward(22);
+
+                will.Right(45);
+                will.Forward(50);
 
 
             }
